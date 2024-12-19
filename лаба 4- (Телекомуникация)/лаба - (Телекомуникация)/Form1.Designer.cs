5c5dec52d874a03b699492184dca3ea70e025f4b
Clarion73 <dolgova.snezhana@list.ru>
2024-12-19 08:30:33 +0400

    Add files via upload

---
+++ лаба 4- (Телекомуникация)/лаба - (Телекомуникация)/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace лаба____Телекомуникация_
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+
+        private System.Windows.Forms.TextBox textBoxRole;
+        private System.Windows.Forms.TextBox textBoxIpAddress;
+        private System.Windows.Forms.TextBox textBoxUserName;
+        private System.Windows.Forms.TextBox textBoxMessage;
+        private System.Windows.Forms.TextBox textBoxChat;
+        private System.Windows.Forms.Button buttonSend;
+
+
+        /// <summary>
+        /// Очистка всех используемых ресурсов.
+        /// </summary>
+        /// <param name="disposing">Указывает, нужно ли освобождать управляемые ресурсы.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        private void InitializeComponent()
+        {
+            this.textBoxRole = new System.Windows.Forms.TextBox();
+            this.textBoxIpAddress = new System.Windows.Forms.TextBox();
+            this.textBoxUserName = new System.Windows.Forms.TextBox();
+            this.textBoxMessage = new System.Windows.Forms.TextBox();
+            this.textBoxChat = new System.Windows.Forms.TextBox();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.labelRole = new System.Windows.Forms.Label();
+            this.labelIpAddress = new System.Windows.Forms.Label();
+            this.labelUserName = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxRole
+            // 
+            this.textBoxRole.Location = new System.Drawing.Point(10, 30);
+            this.textBoxRole.Name = "textBoxRole";
+            this.textBoxRole.Size = new System.Drawing.Size(100, 22);
+            this.textBoxRole.TabIndex = 0;
+            this.textBoxRole.TextChanged += new System.EventHandler(this.textBoxRole_TextChanged);
+            // 
+            // textBoxIpAddress
+            // 
+            this.textBoxIpAddress.Location = new System.Drawing.Point(10, 80);
+            this.textBoxIpAddress.Name = "textBoxIpAddress";
+            this.textBoxIpAddress.Size = new System.Drawing.Size(150, 22);
+            this.textBoxIpAddress.TabIndex = 1;
+            this.textBoxIpAddress.TextChanged += new System.EventHandler(this.textBoxIpAddress_TextChanged);
+            // 
+            // textBoxUserName
+            // 
+            this.textBoxUserName.Location = new System.Drawing.Point(10, 130);
+            this.textBoxUserName.Name = "textBoxUserName";
+            this.textBoxUserName.Size = new System.Drawing.Size(150, 22);
+            this.textBoxUserName.TabIndex = 2;
+            this.textBoxUserName.TextChanged += new System.EventHandler(this.textBoxUserName_TextChanged);
+            // 
+            // textBoxMessage
+            // 
+            this.textBoxMessage.Location = new System.Drawing.Point(10, 391);
+            this.textBoxMessage.Name = "textBoxMessage";
+            this.textBoxMessage.Size = new System.Drawing.Size(250, 22);
+            this.textBoxMessage.TabIndex = 5;
+            this.textBoxMessage.TextChanged += new System.EventHandler(this.textBoxMessage_TextChanged);
+            // 
+            // textBoxChat
+            // 
+            this.textBoxChat.Location = new System.Drawing.Point(10, 172);
+            this.textBoxChat.Multiline = true;
+            this.textBoxChat.Name = "textBoxChat";
+            this.textBoxChat.ReadOnly = true;
+            this.textBoxChat.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxChat.Size = new System.Drawing.Size(360, 200);
+            this.textBoxChat.TabIndex = 4;
+            this.textBoxChat.TextChanged += new System.EventHandler(this.textBoxChat_TextChanged);
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(270, 391);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(100, 23);
+            this.buttonSend.TabIndex = 6;
+            this.buttonSend.Text = "Отправить";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // labelRole
+            // 
+            this.labelRole.AutoSize = true;
+            this.labelRole.Location = new System.Drawing.Point(10, 10);
+            this.labelRole.Name = "labelRole";
+            this.labelRole.Size = new System.Drawing.Size(186, 16);
+            this.labelRole.TabIndex = 7;
+            this.labelRole.Text = "Роль (s - сервер, c - клиент):";
+            // 
+            // labelIpAddress
+            // 
+            this.labelIpAddress.AutoSize = true;
+            this.labelIpAddress.Location = new System.Drawing.Point(10, 60);
+            this.labelIpAddress.Name = "labelIpAddress";
+            this.labelIpAddress.Size = new System.Drawing.Size(65, 16);
+            this.labelIpAddress.TabIndex = 8;
+            this.labelIpAddress.Text = "IP-адрес:";
+            // 
+            // labelUserName
+            // 
+            this.labelUserName.AutoSize = true;
+            this.labelUserName.Location = new System.Drawing.Point(10, 110);
+            this.labelUserName.Name = "labelUserName";
+            this.labelUserName.Size = new System.Drawing.Size(132, 16);
+            this.labelUserName.TabIndex = 9;
+            this.labelUserName.Text = "Имя пользователя:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(116, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Подключение";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button
+            // 
+            this.button.Location = new System.Drawing.Point(166, 79);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(75, 23);
+            this.button.TabIndex = 11;
+            this.button.Text = "IP адресс";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(246, 30);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(124, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Отключение";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(960, 500);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxRole);
+            this.Controls.Add(this.textBoxIpAddress);
+            this.Controls.Add(this.textBoxUserName);
+            this.Controls.Add(this.textBoxChat);
+            this.Controls.Add(this.textBoxMessage);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.labelRole);
+            this.Controls.Add(this.labelIpAddress);
+            this.Controls.Add(this.labelUserName);
+            this.Name = "Form1";
+            this.Text = "Чат";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+
+        #endregion
+
+        private System.Windows.Forms.Label labelRole;
+        private System.Windows.Forms.Label labelIpAddress;
+        private System.Windows.Forms.Label labelUserName;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.Button button3;
+    }
+}
---
+++ лаба 4- (Телекомуникация)/лаба - (Телекомуникация)/Form1.cs
@@ -0,0 +1,437 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace лаба____Телекомуникация_
+{
+    public partial class Form1 : Form
+    {
+        private ServerObject server;
+        private ClientObject client;
+
+        private string userName = "Клиент";
+        private string serverName = "Сервер";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string role = textBoxRole.Text.Trim().ToLower();
+            if (role == "s")
+            {
+                try
+                {
+                    server = new ServerObject(AppendTextToChat);
+
+                    string inputServerName = textBoxUserName.Text.Trim();
+                    if (!string.IsNullOrEmpty(inputServerName))
+                    {
+                        serverName = inputServerName;
+                        server.ServerName = serverName;
+                    }
+
+                    Task.Run(() => server.Listen());
+                    AppendTextToChat($"Сервер запущен... Хост {server.ServerName}");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка запуска сервера: {ex.Message}");
+                }
+            }
+            else if (role == "c")
+            {
+                string ipAddress = textBoxIpAddress.Text.Trim();
+                if (string.IsNullOrEmpty(ipAddress))
+                {
+                    MessageBox.Show("Введите IP-адрес сервера.");
+                    return;
+                }
+
+                userName = textBoxUserName.Text.Trim();
+                if (string.IsNullOrEmpty(userName))
+                {
+                    MessageBox.Show("Введите имя пользователя.");
+                    return;
+                }
+
+                try
+                {
+                    client = new ClientObject(ipAddress, 12345, userName, AppendTextToChat);
+                    Task.Run(() => client.Connect());
+                    AppendTextToChat("Подключение к серверу...");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка подключения к серверу: {ex.Message}");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Укажите корректную роль: 's' для сервера или 'c' для клиента.");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string role = textBoxRole.Text.Trim().ToLower();
+            if (role == "s")
+            {
+                try
+                {
+                    string localIP = GetLocalIPAddress();
+                    textBoxIpAddress.Text = localIP;
+                    textBoxIpAddress.ReadOnly = true;
+                    AppendTextToChat($"Ваш IP: {localIP}");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка получения IP-адреса: {ex.Message}");
+                }
+            }
+            else if (role == "c")
+            {
+                textBoxIpAddress.ReadOnly = false;
+                MessageBox.Show("Введите IP-адрес сервера в поле IP.");
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, укажите корректную роль: 's' для сервера или 'c' для клиента.");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string role = textBoxRole.Text.Trim().ToLower();
+            if (role == "s")
+            {
+                server?.Disconnect();
+                AppendTextToChat("Сервер остановлен.");
+            }
+            else if (role == "c")
+            {
+                client?.Close();
+                AppendTextToChat("Клиент отключен.");
+            }
+            else
+            {
+                MessageBox.Show("Укажите корректную роль: 's' для сервера или 'c' для клиента.");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string role = textBoxRole.Text.Trim().ToLower();
+            string message = textBoxMessage.Text.Trim();
+            if (string.IsNullOrEmpty(message))
+            {
+                MessageBox.Show("Введите сообщение.");
+                return;
+            }
+
+            if (role == "s")
+            {
+                string hostMessage = $"{serverName}: {message}";
+                server.BroadcastMessage(hostMessage);
+                AppendTextToChat(hostMessage);
+                textBoxMessage.Clear();
+            }
+            else if (role == "c")
+            {
+                if (client != null && client.IsConnected)
+                {
+                    string fullMessage = $"{userName}: {message}";
+                    client.SendMessage(fullMessage);
+                    AppendTextToChat(fullMessage);
+                    textBoxMessage.Clear();
+                }
+                else
+                {
+                    MessageBox.Show("Клиент не подключен к серверу.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Укажите корректную роль: 's' для сервера или 'c' для клиента.");
+            }
+        }
+
+        public void AppendTextToChat(string message)
+        {
+            if (textBoxChat.InvokeRequired)
+            {
+                textBoxChat.Invoke(new Action(() => textBoxChat.AppendText(message + Environment.NewLine)));
+            }
+            else
+            {
+                textBoxChat.AppendText(message + Environment.NewLine);
+            }
+        }
+
+
+        private string GetLocalIPAddress()
+        {
+            string localIP = "";
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    localIP = ip.ToString();
+                    break;
+                }
+            }
+            if (string.IsNullOrEmpty(localIP))
+            {
+                throw new Exception("Не удалось определить локальный IP-адрес.");
+            }
+            return localIP;
+        }
+
+
+
+        //----------------------------------------------------------------------//
+        private void textBoxChat_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxMessage_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxUserName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void textBoxIpAddress_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxRole_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+    //------------------------------------------------------//
+    public class ServerObject
+    {
+        private UdpClient udpServer;
+        private Action<string> appendTextToChat;
+        private IPEndPoint remoteEndPoint;
+        private string logFilePath;
+
+        public string ServerName { get; set; } = "Сервер";
+
+        public ServerObject(Action<string> appendTextToChatCallback)
+        {
+            appendTextToChat = appendTextToChatCallback;
+
+            string localIP = GetLocalIPAddress();
+            udpServer = new UdpClient(new IPEndPoint(IPAddress.Parse(localIP), 12345));
+
+            // Создаем файл для логирования
+            logFilePath = $"{DateTime.Now:dd.MM.yyyy_HH-mm-ss}.txt";
+            File.Create(logFilePath).Close();
+        }
+
+        public void Listen()
+        {
+            try
+            {
+                appendTextToChat($"Сервер запущен. Ожидание подключений...");
+
+                while (true)
+                {
+                    byte[] receivedData = udpServer.Receive(ref remoteEndPoint);
+                    string message = Encoding.UTF8.GetString(receivedData);
+
+                    // Обработка сообщений
+                    HandleIncomingMessage(message);
+                }
+            }
+            catch (Exception ex)
+            {
+                appendTextToChat($"Ошибка сервера: {ex.Message}");
+            }
+            finally
+            {
+                Disconnect();
+            }
+        }
+
+        private void HandleIncomingMessage(string message)
+        {
+            // Преобразование в двоичный вид
+            string binaryMessage = string.Join(" ", Encoding.UTF8.GetBytes(message)
+                .Select(b => Convert.ToString(b, 2).PadLeft(8, '0')));
+
+            // Логирование сообщения в файл
+            File.AppendAllText(logFilePath, $"{message}\n");
+
+            // Отображение сообщения в разных форматах
+            appendTextToChat($"Получено сообщение:\nЗакодированное: {message}\nДвоичное: {binaryMessage}\nДекодированное: {message}");
+
+            // Пересылка сообщения всем подключенным клиентам
+            BroadcastMessage(message);
+        }
+
+        public void BroadcastMessage(string message)
+        {
+            try
+            {
+                if (remoteEndPoint == null)
+                {
+                    appendTextToChat("Нет подключенных клиентов для отправки сообщения.");
+                    return;
+                }
+                byte[] data = Encoding.UTF8.GetBytes(message);
+                udpServer.Send(data, data.Length, remoteEndPoint);
+            }
+            catch (Exception ex)
+            {
+                appendTextToChat($"Ошибка отправки сообщения: {ex.Message}");
+            }
+        }
+
+        private string GetLocalIPAddress()
+        {
+            string localIP = "";
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    localIP = ip.ToString();
+                    break;
+                }
+            }
+            if (string.IsNullOrEmpty(localIP))
+            {
+                throw new Exception("Не удалось определить локальный IP-адрес.");
+            }
+            return localIP;
+        }
+
+        public void Disconnect()
+        {
+            udpServer?.Close();
+            appendTextToChat("Сервер остановлен.");
+        }
+    }
+
+
+    public class ClientObject
+    {
+        private UdpClient udpClient;
+        private IPEndPoint serverEndPoint;
+        private string userName;
+        private Action<string> appendTextToChat;
+
+        public bool IsConnected { get; private set; } = false;
+
+        public ClientObject(string serverIp, int port, string userName, Action<string> appendTextToChatCallback)
+        {
+            if (string.IsNullOrEmpty(serverIp))
+                throw new ArgumentException("IP-адрес сервера не может быть пустым.", nameof(serverIp));
+            if (port <= 0)
+                throw new ArgumentException("Порт должен быть положительным числом.", nameof(port));
+
+            this.userName = userName ?? "Anonymous";
+            appendTextToChat = appendTextToChatCallback;
+
+            udpClient = new UdpClient();
+            serverEndPoint = new IPEndPoint(IPAddress.Parse(serverIp), port);
+            IsConnected = true;
+        }
+
+        public void Connect()
+        {
+            try
+            {
+                appendTextToChat("Подключение к серверу...");
+                SendMessage($"{userName} подключился.");
+            }
+            catch (Exception ex)
+            {
+                appendTextToChat($"Ошибка при подключении: {ex.Message}");
+                IsConnected = false;
+            }
+        }
+
+        public void SendMessage(string message)
+        {
+            if (!IsConnected)
+            {
+                appendTextToChat("Клиент не подключен.");
+                return;
+            }
+
+            try
+            {
+                byte[] data = Encoding.UTF8.GetBytes(message);
+                udpClient.Send(data, data.Length, serverEndPoint);
+            }
+            catch (Exception ex)
+            {
+                appendTextToChat($"Ошибка отправки сообщения: {ex.Message}");
+            }
+        }
+        public void ReceiveMessage()
+        {
+            try
+            {
+                while (true)
+                {
+                    byte[] receivedData = udpClient.Receive(ref serverEndPoint);
+                    string message = Encoding.UTF8.GetString(receivedData);
+
+                    // Преобразование в двоичный вид
+                    string binaryMessage = string.Join(" ", Encoding.UTF8.GetBytes(message)
+                        .Select(b => Convert.ToString(b, 2).PadLeft(8, '0')));
+
+                    // Вывод в разных форматах
+                    appendTextToChat($"Закодированное: {message}\n Двоичное: {binaryMessage}\n Декодированное: {message}");
+                }
+            }
+            catch (Exception ex)
+            {
+                appendTextToChat($"Ошибка получения сообщения: {ex.Message}");
+            }
+        }
+
+
+        public void Close()
+        {
+            udpClient?.Close();
+            IsConnected = false;
+            appendTextToChat("Клиент отключен.");
+        }
+
+    }
+}
